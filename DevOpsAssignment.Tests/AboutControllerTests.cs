63b047096d9328eb26972f87e86fa361e383e6f7
amit04 <amit04@nagarro.com>
2024-04-02 12:43:40 +0530

    Added Test Cases

---
+++ DevOpsAssignment.Tests/AboutControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using DevOpsAssignment.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DevOpsAssignment.Tests
+{
+    public class AboutControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsOkResult()
+        {
+            // Arrange
+            var controller = new AboutController();
+
+            // Act
+            var result = controller.Get() as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("Welcome to About Controller", result.Value);
+        }
+    }
+}
---
+++ DevOpsAssignment.Tests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using DevOpsAssignment.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DevOpsAssignment.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsOkResult()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Get() as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("Welcome to Home Controller", result.Value);
+        }
+    }
+}
